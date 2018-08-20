2e4c719cffac66331ec11512f962bddc9fdf9654
JuliaKryuchkova <42516136+JuliaKryuchkova@users.noreply.github.com>
2018-08-20 15:21:38 +0300

    Add files via upload

---
+++ Create_list/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            Console.WriteLine("Введите строку");
+            string text = Console.ReadLine();
+            Char delimiter = ' ';
+            String[] words = text.Split(delimiter);
+            foreach (var substring in words)
+            {
+                count = ++count;
+                Console.WriteLine(count + ". " + substring);            
+            }
+            Console.ReadKey();
+        }
+    }
+}
